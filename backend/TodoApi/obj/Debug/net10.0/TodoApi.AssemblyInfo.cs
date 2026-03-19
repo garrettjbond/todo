857f9c21d31e17e2b7c62b143ae482bbc8999f2f
garrettjbond <gbond.dev@gmail.com>
2026-03-18 23:53:47 -0600

    Fully connected backend to frontend so data persists and utilizes database

--- backend/TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
+++ backend/TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2523d65c91428f6c3cf63b441dc2b5f8caca7815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8c346ce6a1fc8f69cbf8ecc911a2a1d0f97f431")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
